847c0e4c7665578c8857e3b9820652e2451089dd
Nayem-51 <nayem20talukdar@gmail.com>
2026-01-12 20:22:22 +0600

    refactor: remove unnecessary blank line in test_admin_api.js

--- test_admin_api.js
+++ test_admin_api.js
@@ -1,10 +1,9 @@
 const axios = require("axios");
-
 // Test token - you'll need to replace this with a valid token from the frontend
 const testAdminAPI = async () => {
   try {
     // First, let's get a valid token by checking what's in the database
-    const response = await axios.get("http://localhost:5000/api/admin/stats", {
+    const response = await axios.get("https://digitallifelessonsa11.web.app/api/admin/stats", {
       headers: {
         Authorization: "Bearer test_token_here",
       },
